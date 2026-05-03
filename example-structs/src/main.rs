ace2bdc9719eaba29b7f88bd466144e163b92d30
Amanat Ali <alph702.12@gmail.com>
2026-05-03 11:06:54 +0500

    Understanded method syntax

--- example-structs/src/main.rs
+++ example-structs/src/main.rs
@@ -3,15 +3,24 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
-fn main() {
-    let rect = Rectangle {
-        width: 30,
-        height: 50,
-    };
 
-    println!("The area of the {:?} is {}", rect, area(&rect));
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+
+    fn can_hold(&self, other: Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn create_rect(width: u32, height: u32) -> Self {
+        Self { width: width, height: height }
+    }
 }
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.width * rect.height
+fn main() {
+    let rect1 = Rectangle::create_rect(50, 80);
+    let rect2 = Rectangle::create_rect(30, 50);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(rect2))
 }
